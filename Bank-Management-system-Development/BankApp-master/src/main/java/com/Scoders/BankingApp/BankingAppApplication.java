191bfb1b2f5b1a132757b7d4e81266d5abe7c9dd
Thapelo Lekhuane <thapelolekhuane2@gmail.com>
2025-04-04 14:52:17 +0200

    import springframewrok.boot.SpringApplication-autoconfigure

--- Bank-Management-system-Development/BankApp-master/src/main/java/com/Scoders/BankingApp/BankingAppApplication.java
+++ Bank-Management-system-Development/BankApp-master/src/main/java/com/Scoders/BankingApp/BankingAppApplication.java
@@ -3,3 +3,4 @@ package com.Scoders.BankingApp;
 import com.Scoders.BankingApp.database.AccountDatabasedummy;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
