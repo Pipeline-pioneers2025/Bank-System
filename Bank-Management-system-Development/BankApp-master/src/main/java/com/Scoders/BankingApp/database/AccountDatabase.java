418cb9ada7d6a17c9d46ab9c71a131ba6b67927e
helma <makhubelehelma@gmail.com>
2025-04-04 09:58:34 +0200

    added a database interactions for account and user models

---
+++ Bank-Management-system-Development/BankApp-master/src/main/java/com/Scoders/BankingApp/database/AccountDatabase.java
@@ -0,0 +1,11 @@
+package com.Scoders.BankingApp.database;
+
+import com.Scoders.BankingApp.model.Account;
+import com.Scoders.BankingApp.model.User;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static com.Scoders.BankingApp.database.UserDatabase.getUserById;
