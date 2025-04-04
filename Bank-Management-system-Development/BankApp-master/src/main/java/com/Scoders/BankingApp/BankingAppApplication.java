04e31726c93919daf6235feaaae227321e14f9ba
Thapelo Lekhuane <thapelolekhuane2@gmail.com>
2025-04-04 14:50:01 +0200

    import springframewrok.boot.SpringApplication

--- Bank-Management-system-Development/BankApp-master/src/main/java/com/Scoders/BankingApp/BankingAppApplication.java
+++ Bank-Management-system-Development/BankApp-master/src/main/java/com/Scoders/BankingApp/BankingAppApplication.java
@@ -2,3 +2,4 @@ package com.Scoders.BankingApp;
 
 import com.Scoders.BankingApp.database.AccountDatabasedummy;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
