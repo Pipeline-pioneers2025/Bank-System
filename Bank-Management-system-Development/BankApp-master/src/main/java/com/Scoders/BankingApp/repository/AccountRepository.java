4f1b4d4abf93ab0e53219071ef0f8a68dc4af581
Thapelo Lekhuane <thapelolekhuane2@gmail.com>
2025-04-04 12:23:53 +0200

    scoders.BankingApp update

--- Bank-Management-system-Development/BankApp-master/src/main/java/com/Scoders/BankingApp/repository/AccountRepository.java
+++ Bank-Management-system-Development/BankApp-master/src/main/java/com/Scoders/BankingApp/repository/AccountRepository.java
@@ -3,4 +3,6 @@ package com.Scoders.BankingApp.repository;
 import com.Scoders.BankingApp.model.Account;
 
 
-public interface AccountRepositor
+public interface AccountRepositor extends com.Scoders.BankingApp.repository.JpaRepository<Account, Long> {
+    Account findByAccNo(Long accNo);
+}
