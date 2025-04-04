1ef7f84e1eeb670e129d6310c700e565d410d000
Thapelo Lekhuane <thapelolekhuane2@gmail.com>
2025-04-04 15:11:38 +0200

    fuction for running main

--- Bank-Management-system-Development/BankApp-master/src/main/java/com/Scoders/BankingApp/BankingAppApplication.java
+++ Bank-Management-system-Development/BankApp-master/src/main/java/com/Scoders/BankingApp/BankingAppApplication.java
@@ -7,4 +7,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class BankingAppApplication
+public class BankingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankingAppApplication.class, args);
+    }
+
+    // Use CommandLineRunner to execute the creation and population logic on startup
+    @Bean
+    public CommandLineRunner run() {
+        return args -> {
+            // Create account table and insert sample accounts when the app starts
+            AccountDatabasedummy.createAccountTable();
+        };
+    }
+}
