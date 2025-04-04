45df61b75a09fc587a1037dc41ae9db0e017ff31
Thapelo Lekhuane <thapelolekhuane2@gmail.com>
2025-04-04 14:53:25 +0200

    import springframewrok.context

--- Bank-Management-system-Development/BankApp-master/src/main/java/com/Scoders/BankingApp/BankingAppApplication.java
+++ Bank-Management-system-Development/BankApp-master/src/main/java/com/Scoders/BankingApp/BankingAppApplication.java
@@ -4,3 +4,4 @@ import com.Scoders.BankingApp.database.AccountDatabasedummy;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
