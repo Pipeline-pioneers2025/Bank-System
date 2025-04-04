f2d6c9ac0dae94988f53ff57ec8fb42621ebcc94
helma <makhubelehelma@gmail.com>
2025-04-04 13:18:51 +0200

    Created Account table

--- Bank-Management-system-Development/BankApp-master/src/main/java/com/Scoders/BankingApp/database/AccountDatabasedummy.java
+++ Bank-Management-system-Development/BankApp-master/src/main/java/com/Scoders/BankingApp/database/AccountDatabasedummy.java
@@ -6,3 +6,15 @@ import com.Scoders.BankingApp.model.User;
 import java.sql.*;
 
 import static com.Scoders.BankingApp.database.UserDatabase.getUserById;
+
+public class AccountDatabasedummy {
+
+    private static final String DATABASE_URL = "jdbc:sqlite:bank.db"; // The SQLite database file
+
+    // Method to create the Account table
+    public static void createAccountTable() {
+        String createTableSQL = "CREATE TABLE IF NOT EXISTS Account ("
+                + "accNo INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + "user_id INTEGER, "
+                + "balance DOUBLE, "
+                + "FOREIGN KEY(user_id) REFERENCES User(id))";
