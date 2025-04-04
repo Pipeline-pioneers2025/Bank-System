89d4779c7c08cd63da033b4d55b00cde3369cb5d
Anesipho-Mazaza <aneshmazaza@gmail.com>
2025-04-04 09:59:18 +0200

    Added Account entity with JPA annotations and User relationship

---
+++ Bank-Management-system-Development/BankApp-master/src/main/java/com/Scoders/BankingApp/model/transaction.java
@@ -0,0 +1,19 @@
+package com.Scoders.BankingApp.model;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class    Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long accNo;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    private Double balance;
