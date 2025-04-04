89cafca97168398e4979e1a675a79bc3cd8b3ab4
Anesipho-Mazaza <aneshmazaza@gmail.com>
2025-04-04 10:37:28 +0200

    Creating Transaction entity with transId, amount, and dateTime fields

---
+++ Bank-Management-system-Development/BankApp-master/src/main/java/com/Scoders/BankingApp/model/transaction.java
@@ -0,0 +1,18 @@
+package com.Scoders.BankingApp.model;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.time.LocalDateTime;
+
+@Entity
+public class transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long transId;
+
+    private Double amount;
+    private LocalDateTime dateTime;
+    
