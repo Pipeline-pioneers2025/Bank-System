54e43c93e3a2bcd6d016dc1c12868640a67c17a6
helma <makhubelehelma@gmail.com>
2025-04-04 13:14:38 +0200

    Added database package with Account and User model interactions

---
+++ Bank-Management-system-Development/BankApp-master/src/main/java/com/Scoders/BankingApp/database/AccountDatabasedummy.java
@@ -0,0 +1,8 @@
+package com.Scoders.BankingApp.database;
+
+import com.Scoders.BankingApp.model.Account;
+import com.Scoders.BankingApp.model.User;
+
+import java.sql.*;
+
+import static com.Scoders.BankingApp.database.UserDatabase.getUserById;
