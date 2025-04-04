10188f651ae9ddb0a611d9b4a6251c797f3ec1ae
helma <makhubelehelma@gmail.com>
2025-04-04 13:49:23 +0200

    Added database package for Account and Transaction models

---
+++ Bank-Management-system-Development/BankApp-master/src/main/java/com/Scoders/BankingApp/database/TransactionDatabase.java
@@ -0,0 +1,11 @@
+package com.Scoders.BankingApp.database;
+
+import com.Scoders.BankingApp.model.Account;
+import com.Scoders.BankingApp.model.transaction;
+
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
