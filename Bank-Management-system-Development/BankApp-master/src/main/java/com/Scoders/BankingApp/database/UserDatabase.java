98165a66f2de9f8441a1099318d7259a682ea234
helma <makhubelehelma@gmail.com>
2025-04-04 14:57:13 +0200

    Created User table in the database

--- Bank-Management-system-Development/BankApp-master/src/main/java/com/Scoders/BankingApp/database/UserDatabase.java
+++ Bank-Management-system-Development/BankApp-master/src/main/java/com/Scoders/BankingApp/database/UserDatabase.java
@@ -3,3 +3,15 @@ package com.Scoders.BankingApp.database;
 import com.Scoders.BankingApp.model.User;
 
 import java.sql.*;
+
+public class UserDatabase {
+
+    private static final String DATABASE_URL = "jdbc:sqlite:bank.db";
+
+    public static void createUserTable() {
+        String createTableSQL = "CREATE TABLE IF NOT EXISTS User ("
+                + "id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + "username TEXT, "
+                + "surname TEXT, "
+                + "password TEXT)";
+
