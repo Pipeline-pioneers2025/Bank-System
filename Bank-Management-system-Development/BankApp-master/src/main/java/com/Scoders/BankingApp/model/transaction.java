f15b5af0d9530111ad05cc36a4d0b12725e442af
Anesipho-Mazaza <aneshmazaza@gmail.com>
2025-04-04 15:39:29 +0200

    transaction funtionalities added

--- Bank-Management-system-Development/BankApp-master/src/main/java/com/Scoders/BankingApp/model/transaction.java
+++ Bank-Management-system-Development/BankApp-master/src/main/java/com/Scoders/BankingApp/model/transaction.java
@@ -14,5 +14,51 @@ public class transaction {
     private Long transId;
 
     private Double amount;
-   
-    
+
+    @ManyToOne
+    @JoinColumn(name = "accNo", referencedColumnName = "accNo")
+    private Account account;  // AccNo Foreign Key relationship
+
+    private String transactionType;
+
+    // Getters and Setters
+    public Long getTransId() {
+        return transId;
+    }
+
+    public void setTransId(Long transId) {
+        this.transId = transId;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    public String getTransactionType() {
+        return transactionType;
+    }
+
+    public void setTransactionType(String transactionType) {
+        this.transactionType = transactionType;
+    }
+}
